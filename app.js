4a6d0a32e3a9232dbf81bdab716597d42470c44e
xnubber <aabbccdd10902@gmail.com>
2021-10-23 17:12:03 +0800

    initial

---
+++ app.js
@@ -0,0 +1,36 @@
+// express
+const express = require('express')
+const app = express()
+const port = 3000
+// restaurant data
+const restaurantList = require('./restaurant.json')
+
+// exprss-handlebars
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// static
+app.use(express.static('public'))
+
+// index page
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// show page
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant: restaurant })
+})
+
+// search
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase()))
+  res.render('index', { restaurants: restaurants, keyword: req.query.keyword })
+})
+
+// listen
+app.listen(port, () => {
+  console.log(`localhost:${port}`)
+})
