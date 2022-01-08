0f8d597eeb5685144f4065ecb73b0f2b8c55f33e
xnubber <aabbccdd10902@gmail.com>
2022-01-08 22:24:28 +0800

    add mongo uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb://localhost:27017/restaurant-list')
 
 const db = mongoose.connection
 db.on('error', () => {
