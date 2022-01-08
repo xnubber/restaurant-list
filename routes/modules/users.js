bf02289fcb05b20d76c8c361cc60f6d3ed8d181b
xnubber <aabbccdd10902@gmail.com>
2022-01-08 19:09:37 +0800

    feat: instant login after register

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -20,16 +20,24 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
   const {name, email, password, confirmPassword} = req.body
   const user = await User.findOne({email})
   if (user) {
     req.flash('warning_msg', 'User already exists.')
     return res.render('register', { name, email, password, confirmPassword })
   }
+  if (password !== confirmPassword) {
+    req.flash('warning_msg', 'Password and confirm password must be the same.')
+    return res.render('register', { name, email, password, confirmPassword })
+  }
   const newUser = new User({ name, email, password })
   await newUser.save()
-  res.redirect('/')
+  req.login(newUser, err =>{
+    if (err) return next(err)
+    req.flash('success_msg', 'Login and Registration success!')
+    res.redirect('/')
+  })
 }))
 
 router.get('/logout', (req, res) => {
